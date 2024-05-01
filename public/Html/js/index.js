59b12e9499cc5b847b3db64a3f2a361dec3dc46d
Olasunkanmi Usman <Olasunkanmiusman1111@gmail.com>
2024-05-01 21:53:12 +0100

    updated skills

--- public/Html/js/index.js
+++ public/Html/js/index.js
@@ -5,13 +5,13 @@ const contact = document.querySelector('#contact');
 
 document.addEventListener('DOMContentLoaded', () => {
     
-    if(location.pathname.includes('/project.html')){
+    if(location.pathname.includes('project.html')){
         projects.classList.add('text-white');
        }
-      else if(location.pathname.includes('/resume.html')){
+      else if(location.pathname.includes('resume.html')){
         resume.classList.add('text-white');
        }   
-      else if(location.pathname.includes('/contact.html')){
+      else if(location.pathname.includes('contact.html')){
         contact.classList.add('text-white');
        } 
        else{
